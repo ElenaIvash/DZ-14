9d323f62489a4178d48bfa9f6582dd0a188f9546
Elena <77905432@mail.ru>
2022-04-20 18:45:03 +0300

    login form implementation

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+/*let num = 1;
+while (num <= 100) {
+    console.log(num);
+    num++;
+}*/
+
+
+/*let num = 100;
+while (num >= 1) {
+    console.log(num);
+    num--;
+}*/
+
+
+/*for (let a = 1; a <= 100; a++) {
+    if (a % 2 == 0) {
+        console.log (a);   
+    }
+}*/
+
+
+/*const arr = [1, 2, 3, 4, 5];
+let result = 0;
+for (let i = 0; i < arr.length; i++) {
+  result += arr[i];
+}
+console.log(result);*/
+
+
+/*const arr = [1, 2, 3, 4, 5];
+let result = 0;
+for (let i = 0; i < arr.length; i++) {
+	result += arr[i] * arr[i];
+}
+console.log(result);*/
